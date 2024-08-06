a4dbaa88be9564116c7497fb4dc948b3a601f2fd
Keigo <sliverkeigo343@gmail.com>
2024-08-06 22:31:33 +0800

    ✨ Update Register and Login pages
    - Refactor Register and Login pages with consistent styles and colors.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 const Login = () => {
 
     return (
-      <div className={"bg-blue-100 h-screen flex items-center justify-center"}>
-        <div className={"flex flex-col rounded-lg gap-2.5 bg-white p-16 py-5 items-center"}>
-          <span className={"text-pink-300 text-xl font-bold"}>Keigo Chat</span>
-          <span className={"text-pink-300 text-xs"}>Login</span>
+      <div className={"bg-gray-100 h-screen flex items-center justify-center"}>
+        <div className={"flex flex-col rounded-lg gap-2.5 bg-white p-16 py-5 items-center shadow-2xl"}>
+          <span className={"text-blue-600 text-xl font-bold"}>Keigo Chat</span>
+          <span className={"text-gray-600 text-xs"}>Login</span>
           <form className={"flex flex-col gap-3.5"}>
-            <input className={"p-4 border-0 border-b border-solid border-purple-100 placeholder-gray-400"} type={"text"}
+            <input className={"p-4 border-0 border-b border-solid border-gray-300 placeholder-gray-500"} type={"text"}
                    placeholder={"email"}/>
-            <input className={"p-4 border-0 border-b border-solid border-purple-100 placeholder-gray-400"}
+            <input className={"p-4 border-0 border-b border-solid border-gray-300 placeholder-gray-500"}
                    type={"password"} placeholder={"password"}/>
-            <button className={"bg-blue-300 text-white p-3 font-bold border-0 cursor-pointer"}>Sign in</button>
+            <button className={"bg-blue-500 text-white p-3 font-bold border-0 cursor-pointer hover:bg-blue-600"}>Sign in</button>
           </form>
 
-          <p className={"text-pink-400 text-sm mt-3"}>
-            You don't have an account? <Link to="/register" className="text-blue-500">Register</Link>
+          <p className={"text-gray-600 text-sm mt-3"}>
+            You don't have an account? <Link to="/register" className="text-blue-500 hover:text-blue-600">Register</Link>
           </p>
         </div>
       </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,27 +2,27 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div className={"bg-blue-100 h-screen flex items-center justify-center"}>
-      <div className={"flex flex-col rounded-lg gap-2.5 bg-white p-20 py-5 items-center"}>
-        <span className={"text-pink-300 text-xl font-bold"}>Keigo Chat</span>
-        <span className={"text-pink-300 text-xs"}>Register</span>
+    <div className={"bg-gray-100 h-screen flex items-center justify-center"}>
+      <div className={"flex flex-col rounded-lg gap-2.5 bg-white p-16 py-5 items-center shadow-2xl"}>
+        <span className={"text-blue-600 text-xl font-bold"}>Keigo Chat</span>
+        <span className={"text-gray-600 text-xs"}>Register</span>
         <form className={"flex flex-col gap-3.5"}>
-          <input className={"p-4 border-0 border-b border-solid border-purple-100 placeholder-gray-400"} type={"text"}
+          <input className={"p-4 border-0 border-b border-solid border-gray-300 placeholder-gray-500"} type={"text"}
                  placeholder={"display name"}/>
-          <input className={"p-4 border-0 border-b border-solid border-purple-100 placeholder-gray-400"} type={"text"}
+          <input className={"p-4 border-0 border-b border-solid border-gray-300 placeholder-gray-500"} type={"text"}
                  placeholder={"email"}/>
-          <input className={"p-4 border-0 border-b border-solid border-purple-100 placeholder-gray-400"}
+          <input className={"p-4 border-0 border-b border-solid border-gray-300 placeholder-gray-500"}
                  type={"password"} placeholder={"password"}/>
-          <input className={"p-4 border-0 border-b border-solid border-purple-100 placeholder-gray-400"} type={"file"}
+          <input className={"p-4 border-0 border-b border-solid border-gray-300 placeholder-gray-500"} type={"file"}
                  id={"file"} style={{display: "none"}}/>
-          <label className={"size-9 flex items-center gap-3.5 text-sm cursor-pointer"} htmlFor={"file"}>
+          <label className={"flex items-center gap-3.5 size-8 text-sm cursor-pointer"} htmlFor={"file"}>
             <img src={"https://image.sliverkeigo.fun/file/fe1e9e41e0c3b3a1758cf.png"}/>
-            <span className={"flex text-purple-400 text-center"}>Upload Avatar</span>
+            <span className={"text-blue-600 text-center"}>Upload Avatar</span>
           </label>
-          <button className={"bg-blue-300 text-white p-3 font-bold border-0 cursor-pointer"}>Sign Up</button>
+          <button className={"bg-blue-500 text-white p-3 font-bold border-0 cursor-pointer hover:bg-blue-600"}>Sign Up</button>
         </form>
-        <p className={"text-pink-400 text-sm mt-3"}>
-          You do have an account? <Link to="/login" className="text-blue-500">Login</Link>
+        <p className={"text-gray-600 text-sm mt-3"}>
+          You do have an account? <Link to="/login" className="text-blue-500 hover:text-blue-600">Login</Link>
         </p>
       </div>
     </div>
